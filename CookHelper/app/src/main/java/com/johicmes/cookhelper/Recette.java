897485d956dc442e2487ada7197af287f30c708d
Daviiiiid <bonsantdavid@gmail.com>
2016-12-05 11:10:44 -0500

    j'ai commencé RecetteView mais c'est pas terminé.

--- CookHelper/app/src/main/java/com/johicmes/cookhelper/Recette.java
+++ CookHelper/app/src/main/java/com/johicmes/cookhelper/Recette.java
@@ -15,21 +15,26 @@ public class Recette {
 
     //associations
     Ingredient[] ingredients;
-    RecipeBuilder recipeBuilder; //je ne suis pas sûr pour cette association -> Je pouvais pas mettre des one-way dans USE, enlève-le
     ListeBruteDeRecette listeBruteDeRecette;
 
-    //constructeur de ListeBruteDeRecette
-    public Recette (VignetteDeRecherche vignette)
+    private void constructeur (String nom, String categorie, String typeDePlat, int tempsDeCuisson, int portions, boolean favoris, int image)
     {
-        nom = vignette.getNom();
-        categorie = vignette.getCategorie();
-        typeDePlat = vignette.getTypeDePlat();
-        image = vignette.getImage();
-        // chuis pas sûr comment j'accède les données en mémoire. -> Passé lors de la construction, working on that, utilise des fausses valeurs pour maintenant
-        tempsDeCuisson = 0;
-        portions = 0;
-        favoris = false;
-        ingredients = new Ingredient[vignette.getNumIngredient()];
+        this.nom = nom;
+        this.categorie = categorie;
+        this.typeDePlat = typeDePlat;
+        this.image = image;
+        this.portions = portions;
+        this.favoris = favoris;
+    }
+
+    public Recette (String nom, String categorie, String typeDePlat, int tempsDeCuisson, int portions, boolean favoris, int image)
+    {
+        constructeur(nom, categorie, typeDePlat, tempsDeCuisson, portions, favoris, image);
+    }
+
+    public Recette ()
+    {
+        constructeur("nom", "categorie", "type de plat", 0, 0, false, 0);
     }
 
     public void afficher()//ca fait quoi afficher? -> On avait besoin de mettre sa pour les diagrammes de séquence, c'est pour afficher la recette dans l'activity, sa se peut que tu va pas l'utiliser though
@@ -37,9 +42,9 @@ public class Recette {
         // Probablement juste se rajouter au activity ou passer à travers le adapter ou quelque chose comme sa.
     }
 
-    public void ajouterIngredient() // me semble que ça c'est pour recipe builder -> Oui, mais faut tu construit les ingrédients et que tu les donne a recette, tu va avoir besoin de cette fonction
+    public void ajouterIngredient(Ingredient[] ingredients) //Cette méthode remplace le array d'Ingrédients
     {
-// SUP DAVE
+        this.ingredients = ingredients;
     }
 
     //getters
--- CookHelper/app/src/main/java/com/johicmes/cookhelper/VignetteDeRecherche.java
+++ CookHelper/app/src/main/java/com/johicmes/cookhelper/VignetteDeRecherche.java
@@ -28,9 +28,7 @@ public class VignetteDeRecherche {
 
     }
 
-    ///////////
-    //getters// <- PLZ don't do this
-    ///////////
+    //getters
     public String getNom()
     {
         return nom;
