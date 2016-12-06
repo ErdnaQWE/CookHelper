6d4f810cd2d51b92e21f0a61db19a2bd4f42a6b6
Daviiiiid <bonsantdavid@gmail.com>
2016-12-06 14:36:37 -0500

    fini RecetteAdapter et crée le listner pour les boutons

---
+++ CookHelper/app/src/main/java/LeListnerQuiListenTout.java
@@ -0,0 +1,24 @@
+import android.content.DialogInterface;
+import android.view.View;
+
+import com.johicmes.cookhelper.R;
+
+/**
+ * Created by Daviiiiid on 2016-12-06.
+ */
+public class LeListnerQuiListenTout implements View.OnClickListener
+{
+    //si tu veux accéder à quelquechose de dynamique ou accéder aux méthodes du view utilise v.
+
+    public void onClick(View v)
+    {
+        switch (v.findViewById(v.getId()).getId())
+        {
+            case R.id.editText7://exemple de comment ca marche, met le id approprié ici
+            {
+                //code ici
+                break;
+            }
+        }
+    }
+}
--- CookHelper/app/src/main/java/com/johicmes/cookhelper/RecetteAdapter.java
+++ CookHelper/app/src/main/java/com/johicmes/cookhelper/RecetteAdapter.java
@@ -28,10 +28,12 @@ public class RecetteAdapter extends ArrayAdapter<Recette> {
         //ImageView thumbnail = (ImageView) rowView.findViewById(R.id.thumbnail);//Il se peut que l'image soit mis dans RecetteActivity Dynamiquement tout comme les étapes
         TextView nomView = (TextView) rowView.findViewById(R.id.textNom);
         TextView categorietTypeDePlatView = (TextView) rowView.findViewById(R.id.textCategorieEtType);
+        TextView description = (TextView) rowView.findViewById(R.id.textView3);
 
         //thumbnail.setImageResource(getItem(position).getImage());
         nomView.setText(getItem(position).getNom());
         categorietTypeDePlatView.setText(getItem(position).getCategorieEtTypeDePlat());
+        description.setText(getItem(position).getDescription());
 
         return rowView;
     }
