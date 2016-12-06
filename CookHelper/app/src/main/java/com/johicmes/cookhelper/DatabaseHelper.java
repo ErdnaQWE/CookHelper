cb6a6a8b10f749664efc8ee0030392b7efd16507
erdnaQWE <adion076@uottawa.ca>
2016-12-05 22:06:55 -0500

    Fixed the DatabaseHelper constructor

--- CookHelper/app/src/main/java/com/johicmes/cookhelper/DatabaseHelper.java
+++ CookHelper/app/src/main/java/com/johicmes/cookhelper/DatabaseHelper.java
@@ -1,5 +1,7 @@
 package com.johicmes.cookhelper;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
@@ -8,6 +10,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
