360d2cdbc3043217b913f3dbcedeef4ee7c75c94
JohicMes <johicmes@gmail.com>
2016-12-06 01:46:15 -0500

    - Just added the on click de terminer lors de la création d'une recette... ça send maintenant à recipe view.

--- CookHelper/app/src/main/java/com/johicmes/cookhelper/RecipeCreate.java
+++ CookHelper/app/src/main/java/com/johicmes/cookhelper/RecipeCreate.java
@@ -1,11 +1,13 @@
 package com.johicmes.cookhelper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class RecipeCreate extends AppCompatActivity {
 
@@ -15,5 +17,15 @@ public class RecipeCreate extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_create);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button terminer = (Button) findViewById(R.id.buttonDone);
+        terminer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecipeCreate.this, RecipeView.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
